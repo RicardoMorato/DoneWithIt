857a87fa2a570e3d7287f59592453c852e9f2628
Ricardo Morato Rocha <ricardomorator@hotmail.com>
2020-08-02 00:07:32 -0300

    main(WelcomeScreen): Added basic style.

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,8 +1,22 @@
 import React from 'react';
-import './Component.scss';
+import { ImageBackground, View, Image, Text } from 'react-native';
+import styles from './styles';
 
-export default function Component() {
+export default function WelcomeScreen() {
   return (
-    <p>Dale boy!</p>
+    <ImageBackground
+      source={require('../assets/background.jpg')}
+      style={styles.background}
+    >
+      <View style={styles.logoContainer}>
+        <Image
+          source={require('../assets/logo-red.png')}
+          style={styles.logoImage}
+        />
+        <Text>Sell What You Don't Need</Text>
+      </View>
+      <View style={styles.loginButton} />
+      <View style={styles.registerButton} />
+    </ImageBackground>
   );
 }
---
+++ src/screens/styles.js
@@ -0,0 +1,31 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+  logoContainer: {
+    position: 'absolute',
+    top: 70,
+    alignSelf: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  logoImage: {
+    width: 100,
+    height: 100,
+  },
+  loginButton: {
+    width: '100%',
+    height: 70,
+    backgroundColor: '#fc5c65'
+  },
+  registerButton: {
+    width: '100%',
+    height: 70,
+    backgroundColor: '#4ecdc4'
+  },
+})
+
+export default styles;
