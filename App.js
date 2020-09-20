2ca0d3ef0a0e00f4dd285db8e4208b992b4a0ed9
Ricardo Morato Rocha <ricardomorator@hotmail.com>
2020-09-20 00:27:55 -0300

    main(LoginScreen): Added new screen.

--- App.js
+++ App.js
@@ -1,29 +1,8 @@
 import React, { useState } from 'react';
-
-import { ListingScreen } from './src/screens/ListingScreen';
-import { AppTextInput } from './src/components/AppTextInput';
-import { AppPicker } from './src/components/AppPicker';
-import { Screen } from './src/components/Screen';
-
-const categories = [
-  { label: 'Furniture', value: 1 },
-  { label: 'Clothing', value: 2 },
-  { label: 'Cameras', value: 3 },
-];
+import { LoginScreen } from './src/screens/LoginScreen';
 
 export default function App() {
-  const [category, setCategory] = useState(categories[0]);
-
   return (
-    <Screen>
-      <AppPicker
-        selectedItem={category}
-        onSelectedItem={item => setCategory(item)}
-        items={categories}
-        placeholder='Category'
-        icon='apps'
-      />
-      <AppTextInput placeholder='Email' icon='email' />
-    </Screen>
+    <LoginScreen />
   );
 }
